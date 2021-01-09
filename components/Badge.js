85d9e6eb00c66625d171799160aac4cc5f2b733f
MohammedAbuYousef <mohammedyousef0403@gmail.com>
2021-01-09 18:26:07 +0200

    first message

---
+++ components/Badge.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { StyleSheet } from "react-native"
+
+import Block from "./Block"
+import { theme } from "../constants"
+
+class Badge extends React.Component {
+  render() {
+    const { children, style, size, color, ...props } = this.props
+
+    const badgeStyles = StyleSheet.flatten([
+      styles.badge,
+      size && {
+        height: size,
+        width: size,
+        borderRadius: size
+      },
+      style
+    ])
+
+    return (
+      <Block
+        flex={false}
+        middle
+        center
+        color={color}
+        style={badgeStyles}
+        {...props}
+      >
+        {children}
+      </Block>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  badge: {
+    height: theme.sizes.base,
+    width: theme.sizes.base,
+    borderRadius: theme.sizes.border
+  }
+})
+
+export default  Badge
---
+++ components/Card.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { StyleSheet } from "react-native"
+
+import Block from "./Block"
+import { theme } from "../constants"
+
+class Card extends React.Component {
+  render() {
+    const { color, style, children, ...props } = this.props
+    const cardStyles = [styles.card, style]
+
+    return (
+      <Block color={color || theme.colors.white} style={cardStyles} {...props}>
+        {children}
+      </Block>
+    )
+  }
+}
+
+export const styles = StyleSheet.create({
+  card: {
+    borderRadius: theme.sizes.border,
+    padding: theme.sizes.base + 4,
+    marginBottom: theme.sizes.base
+  },
+  shadow: {
+    shadowColor: theme.colors.black,
+    shadowOpacity: 0.11,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 13
+  }
+})
+
+export default Card
